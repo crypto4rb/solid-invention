f4e7f58ea3c7687d73be8d2bec20a3bf21d539b8
R B <41969592+crypto4rb@users.noreply.github.com>
2026-09-12 14:13:33 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,3 @@
+from ai_architect.crew import ClarifierCrew, AiArchitect
+
+decision = ClarifierCrew().run("Build an Airbnb-style marketplace for renting photography studios. Hosts list spaces, photographers book them, payments split between host and platform.")
